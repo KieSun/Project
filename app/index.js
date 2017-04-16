ef8198ea8a103f1034843b07e11b193fe8819637
yuchengkai <zx597813039@gmail.com>
2017-04-16 09:41:32 +0800

    jquery

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
-import _ from 'lodash';
+import j from 'jquery'
+import _ from 'lodash'
+
+
 function component () {
-    var element = document.createElement('div');
+    var element = j('<div></div>');
 
     /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
+    element.html(_.join(['Hello','webpack'], ' '));
 
-    return element;
+    return element.get(0);
 }
 
 document.body.appendChild(component());
