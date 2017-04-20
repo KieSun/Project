bdc3b89ea307a162992c38da41e1b31e89d888dc
俞诚恺 <zx597813039@gmail.com>
2017-04-20 13:39:51 +0800

    Fix value again

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -13,8 +13,6 @@ class App extends Component {
     this.state = {
       newTodo: '',
       todoList: [
-        {id: 1, title: '第一个待办'},
-        {id: 2, title: '第二个待办'}
       ]
     }
   }
@@ -23,7 +21,7 @@ class App extends Component {
 
     let todos = this.state.todoList.map( (item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       )
@@ -33,7 +31,9 @@ class App extends Component {
       <div className="App">
         <h1>我的代办</h1>
         <div className='inputWrapper'>
-          <TodoInput content={this.state.newTodo}/>
+          <TodoInput content={this.state.newTodo} 
+                     onChange={this.changeTitle.bind(this)}
+                     onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -41,6 +41,31 @@ class App extends Component {
       </div>
     );
   }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      state: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;
+
+let id = 0;
+
+function idMaker() {
+  id += 1
+  return id
+}
--- my-react/src/TodoInput.js
+++ my-react/src/TodoInput.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 class TodoInput extends Component {
 
     render() {
-        return <input type="text" defaultValue={this.props.content} onKeyPress={this.submit}/>
+        return <input type="text" value={this.props.content} 
+                onChange={this.changeTitle.bind(this)}
+                onKeyPress={this.submit.bind(this)}
+                />
     }
     submit(e) {
         if (e.key === 'Enter') {
-            console.log("enter")
+            this.props.onSubmit(e)
         }
+    }
+    changeTitle(e) {
+        this.props.onChange(e)
     }
 }
 
